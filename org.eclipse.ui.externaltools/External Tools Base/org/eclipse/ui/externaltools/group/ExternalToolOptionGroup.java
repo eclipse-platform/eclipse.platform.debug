22ecb4a238a5b753861f6daa056c32a4ea4837b0
Jared Burns <jburns>
2002-10-30 20:42:27 +0000

    Removed unused imports

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/group/ExternalToolOptionGroup.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/group/ExternalToolOptionGroup.java
@@ -10,10 +10,8 @@ Contributors:
 **********************************************************************/
 
 import java.text.Collator;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
