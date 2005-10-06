bb21483a519eaf6d2f57e997cfcba90b4faeeec7
Darin Wright <darin>
2005-10-06 01:59:53 +0000

    remap elements when set selected elements are equal but not identical to selection. Handles synch issue between setSelection & refresh, so correct element is selected/sent in selection change

--- org.eclipse.debug.ui/ui/org/eclipse/debug/ui/viewers/AsynchronousTreeViewer.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/ui/viewers/AsynchronousTreeViewer.java
@@ -622,6 +622,7 @@ public class AsynchronousTreeViewer extends AsynchronousViewer {
 	protected synchronized ISelection doAttemptSelectionToWidget(ISelection selection, boolean reveal) {
 		List remaining = new ArrayList();
 		List toSelect = new ArrayList();
+		List theElements = new ArrayList();
 		TreeSelection treeSelection = (TreeSelection) selection;
 		TreePath[] paths = treeSelection.getPaths();
 		for (int i = 0; i < paths.length; i++) {
@@ -636,6 +637,7 @@ public class AsynchronousTreeViewer extends AsynchronousViewer {
 					TreePath existingPath = treePaths[j];
 					if (existingPath.equals(path)) {
 						toSelect.add(existingPath.getTreeItem());
+						theElements.add(path.getLastSegment());
 						selected = true;
 						break;
 					}
@@ -647,7 +649,16 @@ public class AsynchronousTreeViewer extends AsynchronousViewer {
 		}
 		if (!toSelect.isEmpty()) {
 			final TreeItem[] items = (TreeItem[]) toSelect.toArray(new TreeItem[toSelect.size()]);
-
+			// TODO: hack to ensure selection contains 'selected' element
+			// instead of 'equivalent' element. Handles synch problems between
+			// set selection & refresh
+			for (int i = 0; i < items.length; i++) {
+				TreeItem item = items[i];
+				Object element = theElements.get(i);
+				if (item.getData() != element) {
+					remap(element, item);
+				}
+			}
 			fTree.setSelection(items);
 			if (reveal) {
 				fTree.showItem(items[0]);
