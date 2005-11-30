e0c7f8b82e1c1e86c9486026178ec037b15875ab
Darin Wright <darin>
2005-11-30 19:47:18 +0000

    Bug 105071 - Incorrect handling for debugger's continue-launch-despite-compile-errors dialog

--- org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/CompileErrorPromptStatusHandler.java
+++ org.eclipse.debug.ui/ui/org/eclipse/debug/internal/ui/launchConfigurations/CompileErrorPromptStatusHandler.java
@@ -56,7 +56,7 @@ public class CompileErrorPromptStatusHandler implements IStatusHandler {
 		dialog.open();
 		
 		int returnValue = dialog.getReturnCode();
-		if (returnValue == IDialogConstants.OK_ID) {
+		if (returnValue == IDialogConstants.YES_ID) {
 			return new Boolean(true);
 		}
 		return new Boolean(false);
