6c67ce7ea655cfa6854f56a91deb0e11519b841a
Darin Wright <darin>
2009-11-03 15:08:15 +0000

    [r352] Bug 293928 - Need ability to disable status handlers

--- org.eclipse.debug.core/core/org/eclipse/debug/core/DebugPlugin.java
+++ org.eclipse.debug.core/core/org/eclipse/debug/core/DebugPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -59,6 +59,7 @@ import org.eclipse.debug.internal.core.DebugCoreMessages;
 import org.eclipse.debug.internal.core.DebugOptions;
 import org.eclipse.debug.internal.core.ExpressionManager;
 import org.eclipse.debug.internal.core.IConfigurationElementConstants;
+import org.eclipse.debug.internal.core.IInternalDebugCoreConstants;
 import org.eclipse.debug.internal.core.LaunchManager;
 import org.eclipse.debug.internal.core.LogicalStructureManager;
 import org.eclipse.debug.internal.core.MemoryBlockManager;
@@ -536,6 +537,10 @@ public class DebugPlugin extends Plugin {
 	 * @since 2.0
 	 */
 	public IStatusHandler getStatusHandler(IStatus status) {
+		boolean enabled = getPluginPreferences().getBoolean(IInternalDebugCoreConstants.PREF_ENABLE_STATUS_HANDLERS);
+		if (!enabled) {
+			return null;
+		}
 		StatusHandlerKey key = new StatusHandlerKey(status.getPlugin(), status.getCode());
 		if (fStatusHandlers == null) {
 			initializeStatusHandlers();
