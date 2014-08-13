f20ee4a4727f554203e45cd8c338e52f54960d08
Mike Rennie <Michael_Rennie@ca.ibm.com>
2014-08-13 07:51:53 -0500

    Bug 437193 - JSR-45 Support broken
    Change-Id: I89aa19f22f284b506c3bdbac8a19b8f958fefd64

--- org.eclipse.debug.tests/src/org/eclipse/debug/tests/AutomatedSuite.java
+++ org.eclipse.debug.tests/src/org/eclipse/debug/tests/AutomatedSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 IBM Corporation and others.
+ * Copyright (c) 2009, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,33 +33,37 @@ import org.eclipe.debug.tests.viewer.model.VirtualViewerSelectionTests;
 import org.eclipe.debug.tests.viewer.model.VirtualViewerStateTests;
 import org.eclipe.debug.tests.viewer.model.VirtualViewerUpdateTests;
 import org.eclipse.debug.tests.breakpoint.BreakpointOrderingTests;
+import org.eclipse.debug.tests.sourcelookup.SourceLookupFacilityTests;
 import org.eclipse.debug.tests.statushandlers.StatusHandlerTests;
 
 /**
   * Tests for integration and nightly builds.
- * 
- * @since 3.6 
+ *
+ * @since 3.6
  */
 public class AutomatedSuite extends TestSuite {
 
 	/**
 	 * Returns the suite.  This is required to use the JUnit Launcher.
-	 * 
+	 *
 	 * @return the test suite
 	 */
 	public static Test suite() {
 		return new AutomatedSuite();
 	}
-	
+
 	/**
-	 * Constructs the automated test suite. Adds all tests. 
+	 * Constructs the automated test suite. Adds all tests.
 	 */
 	public AutomatedSuite() {
+		// Source lookup tests
+		addTest(new TestSuite(SourceLookupFacilityTests.class));
+		// BP tests
 		addTest(new TestSuite(BreakpointOrderingTests.class));
-	    // Note: jface viewer tests were moved out of nightly tests 
-	    // due to frequent problems on nightly build machines. 
-	    // (Bug 343308). 
-		
+	    // Note: jface viewer tests were moved out of nightly tests
+	    // due to frequent problems on nightly build machines.
+	    // (Bug 343308).
+
 		// Virtual viewer tests
 		addTest(new TestSuite(VirtualViewerDeltaTests.class));
         addTest(new TestSuite(VirtualViewerContentTests.class));
@@ -68,15 +72,15 @@ public class AutomatedSuite extends TestSuite {
 		addTest(new TestSuite(VirtualViewerStateTests.class));
 		addTest(new TestSuite(VirtualViewerUpdateTests.class));
         addTest(new TestSuite(VirtualViewerFilterTests.class));
-		
+
 		// Viewer neutral tests
 		addTest(new TestSuite(FilterTransformTests.class));
 		addTest(new TestSuite(ChildrenUpdateTests.class));
 		addTest(new TestSuite(PresentationContextTests.class));
-		
+
 		// Memory view
 		addTest(new TestSuite(MemoryRenderingTests.class));
-		
+
 		// Launch framework
 		addTest(new TestSuite(LaunchConfigurationTests.class));
 		addTest(new TestSuite(AcceleratorSubstitutionTests.class));
@@ -85,7 +89,7 @@ public class AutomatedSuite extends TestSuite {
 		addTest(new TestSuite(LaunchManagerTests.class));
 		addTest(new TestSuite(RefreshTabTests.class));
 		addTest(new TestSuite(ArgumentParsingTests.class));
-		
+
 		// Status handlers
 		addTest(new TestSuite(StatusHandlerTests.class));
 	}
